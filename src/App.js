33d82f0e0272baeaaeed379137ddead9b8da05f0
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-22 17:20:08 +0300

    navbar changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './pages/About';
 
 function App() {
   return (
     <BrowserRouter className="minh-h-screen">
+      <Navbar />
       <Routes>
         <Route exact path='/' element={<Home/>}/>
+        <Route path='/about' element={<About />}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { MdOutlineLightMode } from "react-icons/md";
 
 const Navbar = () => {
+    //<BsFillMoonStarsFill />
+    //<MdOutlineLightMode />
   return (
-    <div>Navbar</div>
+    <section>
+        <h1 className='ml-0 p-3 text-2xl font-semibold'>Question Form</h1>
+        <nav className='flex justify-center items-center'>
+            <NavLink to='/about'>About</NavLink>
+        </nav>
+    </section>
   )
 }
 
