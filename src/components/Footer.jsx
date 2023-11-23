f4d6d1e78ca4fabad8e20069b4654b3d7e50f8c4
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-23 23:47:40 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { FaRegCopyright } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-slate-200'>
+      <p className='h-5 flex justify-center'>You've got questions I have the answers to.</p>
+      <p className='flex justify-center items-center gap-2 mt-4'><span><FaRegCopyright /></span> Questions Inc.</p>
+    </footer>
   )
 }
 
