d5f859c3bd87a5b329ab3f09c4bbe4329d9fd62d
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-23 23:30:35 +0300

    navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { MdOutlineLightMode } from "react-icons/md";
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    //<BsFillMoonStarsFill />
-    //<MdOutlineLightMode />
+    //
   return (
-    <section>
-        <h1 className='ml-0 p-3 text-2xl font-semibold'>Question Form</h1>
-        <nav className='flex justify-center items-center'>
-            <NavLink to='/about'>About</NavLink>
+    <section className='flex justify-center p-3 items-center'>
+        <h1 className='mr-0 pr-2 items-center text-2xl font-semibold'>Question Form</h1>
+        <nav className='flex justify-end items-center gap-3 ml-4'>
+          <MdOutlineLightMode />
+          <BsFillMoonStarsFill />
+          <NavLink className='text-lg' to='/about'>About</NavLink>
         </nav>
     </section>
   )
