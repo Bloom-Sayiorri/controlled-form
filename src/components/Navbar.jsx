6b3285204f42871a4f145940ea06493c41e80540
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-23 23:57:53 +0300

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     //
   return (
-    <section className='flex justify-center p-3 items-center'>
-        <NavLink >
-          <h1 className='mr-0 pr-2 items-center text-2xl font-semibold'>Question Form</h1>
+    <section className='flex justify-between p-3 items-center'>
+        <NavLink to='/'>
+          <h1 className='items-center text-2xl font-semibold'>Questions</h1>
         </NavLink>
-        <nav className='flex justify-end items-center gap-3 ml-4'>
+        <nav className='flex justify-end items-center gap-3 p-2'>
           <MdOutlineLightMode />
           <BsFillMoonStarsFill />
           <NavLink className='text-lg' to='/about'>About</NavLink>
