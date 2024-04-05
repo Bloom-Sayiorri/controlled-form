ce2b6a4877a88e9d3ce7691ea229ec8a8b826192
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-04-05 18:45:42 +0300

    questions fetched from api successfully

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import questions from '../questions'
 
 const Questions = () => {
-  const displayedQuestion = questions.map((question) => {
+  const [questionData, setQuestionData] = useState({
+    choices: ''
+  })
+  const url = "https://opentdb.com/api.php?amount=30&difficulty=medium&type=multiple"
+
+  // const displayed = useEffect(() => {
+  //   fetch(url)
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }, [question])
+
+
+
+  const displayedQuestion = displayed.map((q) => {
     return(
-      <section className='' key={question.id}>
-        <h3>{question.prompt}</h3>
+      <div className='' key={q.id}>
+        <h3>Question: {q.question}</h3>
+        <select
+          id="type"
+          name="choices"
+          value={questionData.choices}
+          onChange={(e) => setQuestionData(e.target.value)}
+        >
+          <option value="free">Free</option>
+        </select>
+        <p>Category: {q.category}</p>
+        <p>Correct_answer: {q.correct_answer}</p>
+        <p>Difficulty: {q.difficulty}</p>
+        <p>Category: {q.category}</p>
         <input type="radio" name="itzem" id="item" value="item" />
-      </section>
+      </div>
     )
   })
   return (
