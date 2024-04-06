20c7c6ec9c184ec2e4ff06016c3c7e0ebfa85694
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-04-06 19:18:12 +0300

    rendering questions done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import Question from './Question.jsx'
+// import Question from './Question.jsx'
+import questions from '../questions'
 
 const Home = () => {
+  const display = questions.map((q) => {
+    return (
+      <div>
+        <h3 className='text-2xl font-bold'>{q.prompt}</h3>
+        <ul>
+            {q.answers.map((ans)=> {
+              return (
+                <li key={ans.text}>
+                  <label>{ans.text}</label>
+                  <input type='radio' name='answers' value={ans.isCorrect} />
+                </li>
+              )
+            })}
+        </ul>
+      </div>
+    )
+  })
 
-  // const rigthAnswer = () => {
-  //   if (answers.length > 0){
-  //     return
-  //   }
-  // }
   return (
-    <Question />
+    // <Question />
+    <>{display}</>
   )
 }
 
--- src/questions.js
+++ src/questions.js
@@ -2,7 +2,12 @@ const questions = [
     {
         id: 1,
         prompt: "React was introduced in which year?",
-        answers: 2000,
+        answers: [
+            { text: "2000", isCorrect: false },
+            { text: "2010", isCorrect: false },
+            { text: "2013", isCorrect: true },
+            { text: "2020", isCorrect: false },
+        ]
     }
 ]
 
