9c463c34edc35aaf69dafa869f7f5f9747e80d29
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-04-08 10:55:18 +0300

    handleChange function in place

--- src/pages/QuestionForm.jsx
+++ src/pages/QuestionForm.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const QuestionForm = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+  const [ formData, setFormData ] = useState({
+    id: 1,
+    question: '',
+    correctAnswer: [
+      { answer1: '' },
+      { answer2: '' },
+      { answer3: '' },
+      { answer4: '' }
+    ]
+  })
+
+  const handleChange = (e) => {
+    const name = e.taget.name
+    const value = e.target.type === 'checked' ? e.target.checked : e.target.value
+    setFormData({
+        ...formData,
+        [name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  };
 
   return (
     <form className='' onSubmit={handleSubmit}>
-
+      <label htmlFor='question'>Question</label>
+      <input 
+        type='text'
+        id='question'
+        name='question'
+        value={formData.question}
+        onChange={handleChange}
+      />
+      <label htmlFor='answer'>Answer</label>
+      <input 
+        type='checkbox'
+        id='answer'
+        name='answer'
+        value={formData.answer}
+        onChange={handleChange}
+      />
     </form>
   )
 }
