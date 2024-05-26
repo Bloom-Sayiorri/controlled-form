a73acfa54f187696257aac26de40d38c6557c7f1
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-05-26 23:03:52 +0300

    cleared up desolete file Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,9 @@
-import React from 'react'
-import QuestionList from './QuestionList.jsx'
-import questions from '../questions'
+import React from 'react';
 
 const Home = () => {
-  const display = questions.map((q) => {
-    return (
-      <div>
-        <h3 className='text-2xl font-bold'>{q.prompt}</h3>
-        <ul>
-            {q.answers.map((ans)=> {
-              return (
-                <li key={ans.text}>
-                  <label>{ans.text}</label>
-                  <input type='radio' name='answers' value={ans.isCorrect} />
-                </li>
-              )
-            })}
-        </ul>
-      </div>
-    )
-  })
 
   return (
-    // <Question />
-    // <>{display}</>
-    <QuestionList />
+    <div></div>
   )
 }
 
